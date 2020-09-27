8ffbd54bc8760ba7bc53b7fb64343ce3833e29d3
Komal Sharma <komal.sharma@catseyesystems.com>
2020-09-27 17:54:34 +0530

    Flip Coin Combinations use case 2

--- flipCoinCombinations.sh
+++ flipCoinCombinations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 #1#
+
 randomValue=$((RANDOM%2));
 echo $randomValue;
 if [ $randomValue == 1 ]
@@ -9,3 +10,48 @@ then
 else
 	echo "Its Tails!!";
 fi
+
+#2#
+
+declare -A singletDict
+hCounter=0;
+tCounter=0;
+
+hhCounter=0;
+ttCounter=0;
+thCounter=0;
+htCounter=0;
+
+function generateHeadsOrTails(){
+	case $1 in
+		1)randomValue="T";;
+		*)randomValue="H";;
+	esac
+	echo $randomValue;
+}
+for i in {1..100}
+do
+	randomValueGenerated=$( generateHeadsOrTails $((RANDOM%2)) );
+	echo $randomValueGenerated;
+	if [ "$randomValueGenerated" == "H" ]
+	then
+		hCounter=$((hCounter+1));
+	else
+		tCounter=$((tCounter+1));
+	fi
+
+	singletDict[$i]=$randomValueGenerated;
+done
+
+headpercent1=`echo $hCounter | awk '{print $1/100}'`;
+headpercent2=`echo $headpercent1 | awk '{print $1*100 }'`;
+
+tailPercent1=`echo $tCounter | awk '{print $1/100}'`;
+tailPercent2=`echo $tailPercent1 | awk '{print $1*100}'`;
+
+echo "Percentage For Heads = "$headpercent2"%";
+echo "Percentage For Tails = "$tailPercent2"%";
+echo "${singletDict[@]}";
+
+
+
